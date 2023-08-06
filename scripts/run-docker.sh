bacce75617b08f85e72ef93a342023742311355b
painebenjamin <57536852+painebenjamin@users.noreply.github.com>
2023-08-06 16:59:44 -0400

    break up image editor into multiple files, update run-docker script

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 CACHE_DIR=$(realpath ~/.cache)
 PORT=45554
-IMAGE=ghcr.io/painebenjamin/app.enfugue.ai:0.2.1
+IMAGE=ghcr.io/painebenjamin/app.enfugue.ai:latest
 docker run --rm --gpus all --runtime=nvidia -v ${CACHE_DIR}:/root/.cache -p ${PORT}:${PORT} -e LOGGING_LEVEL='DEBUG' ${IMAGE} $@
