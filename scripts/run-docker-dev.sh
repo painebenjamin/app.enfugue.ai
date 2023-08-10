e750c083fc8f6f9111387029b63e1818757fe77b
painebenjamin <57536852+painebenjamin@users.noreply.github.com>
2023-08-10 15:59:08 -0400

    Drop down to non-root in docker

---
+++ scripts/run-docker-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+CACHE_DIR=$(realpath ~/.cache)
+PORT=45554
+IMAGE=enfugue
+docker run --rm --gpus all --runtime=nvidia -v ${CACHE_DIR}:/home/enfugue/.cache -p ${PORT}:${PORT} -e LOGGING_LEVEL='DEBUG' ${IMAGE} run
