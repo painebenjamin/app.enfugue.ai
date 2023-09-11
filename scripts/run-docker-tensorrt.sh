c68ca3e3f1814a3c49edf13acfd3c53bdb4cf710
painebenjamin <57536852+painebenjamin@users.noreply.github.com>
2023-09-11 16:33:59 -0400

    add trt run script

---
+++ scripts/run-docker-tensorrt.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+CACHE_DIR=$(realpath ~/.cache)
+PORT=45554
+IMAGE=ghcr.io/painebenjamin/app.enfugue.ai.tensorrt:latest
+docker run --rm --gpus all --runtime=nvidia -v ${CACHE_DIR}:/home/enfugue/.cache -p ${PORT}:${PORT} -e LOGGING_LEVEL='DEBUG' ${IMAGE} run
