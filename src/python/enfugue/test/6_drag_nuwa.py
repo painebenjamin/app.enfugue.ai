32105b6cc843db7ba717880b7c9e968b74ce4bde
Benjamin Paine <painebenjamin@gmail.com>
2024-01-11 15:01:56 -0500

    change requirements for dragnuwa windows build, change test

--- src/python/enfugue/test/6_drag_nuwa.py
+++ src/python/enfugue/test/6_drag_nuwa.py
@@ -28,32 +28,30 @@ def main() -> None:
 
     with DebugUnifiedLoggingContext():
         manager = DiffusionPipelineManager()
-        with manager.drag_animator.nuwa() as drag_nuwa:
-            input_image = image_from_uri(os.path.join(input_dir, "nuwa.png"))
-            frames = drag_nuwa(
-                input_image,
-                motion_vectors=[
-                    [
-                        {
-                            "anchor": (x1, y1)
-                        },
-                        {
-                            "anchor": (x2, y2)
-                        }
-                    ]
-                    for ((x1, y1), (x2, y2)) in [
-                        ((222, 55), (0, 0)),
-                        ((519, 21), (575, 0)),
-                        ((205, 197), (0, 319)),
-                        ((527, 180), (575, 319))
-                    ]
+        frames = manager.dragnuwa_img2vid(
+            image_from_uri("file://" + os.path.join(input_dir, "nuwa.png")),
+            motion_vectors=[
+                [
+                    {
+                        "anchor": (x1, y1)
+                    },
+                    {
+                        "anchor": (x2, y2)
+                    }
                 ]
-            )[0]
-            Video(frames).save(
-                os.path.join(output_dir, "output.gif"),
-                overwrite=True,
-                rate=8.0
-            )
+                for ((x1, y1), (x2, y2)) in [
+                    ((222, 55), (0, 0)),
+                    ((519, 21), (575, 0)),
+                    ((205, 197), (0, 319)),
+                    ((527, 180), (575, 319))
+                ]
+            ]
+        )
+        Video(frames).save(
+            os.path.join(output_dir, "output.gif"),
+            overwrite=True,
+            rate=8.0
+        )
 
 if __name__ == "__main__":
     main()
